7979932b8c7c9f05f39559fb15262f2e3cdeaa45
grozzniy <118670038+grozzniy@users.noreply.github.com>
2022-11-20 17:19:42 +0300

    v1.1
    Добавлен класс GameSprite.

--- ping-pong.py
+++ ping-pong.py
@@ -7,6 +7,17 @@ background = (255,255,255)
 w.fill(background)
 display.set_caption('Пинг-понг.')
 
+class GameSprite(sprite.Sprite):
+    def __init__ (self,player_image,player_x,player_y,size_x,size_y,player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        w.blit(self.image, (self.rect.x,self.rect.y))
+
 game = True
 clock = time.Clock()
 FPS = 60
